5dc37966c2a00b893b695a36732528133f824113
LuisEncastin <luisencastin@gmail>
2022-03-11 13:02:49 -0600

    Handle events

--- src/components/TodoSearch/TodoSearch.js
+++ src/components/TodoSearch/TodoSearch.js
@@ -2,8 +2,13 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+    const onChangeValueInput = (event) => {console.log(event.target.value)}
+
     return (
-        <input className="TodoSearch" placeholder='Buscar tarea'/>
+        <input
+        onChange={onChangeValueInput}
+        className="TodoSearch" 
+        placeholder='Buscar tarea'/>
     );
   }
   
